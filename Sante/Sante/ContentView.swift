b88e6f4aa12d015cab9018bfde0a037e174c92bd
Victor Augusto de Lima Ramos <victoraugustolr@gmail.com>
2026-07-13 15:55:42 -0400

    Add Xcode project

---
+++ Sante/Sante/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Sante
+//
+//  Created by Victor Augusto de Lima Ramos on 2026-07-13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Sante/Sante/SanteApp.swift
@@ -0,0 +1,17 @@
+//
+//  SanteApp.swift
+//  Sante
+//
+//  Created by Victor Augusto de Lima Ramos on 2026-07-13.
+//
+
+import SwiftUI
+
+@main
+struct SanteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
